6a7fda2c03cdfbd3d601c5f14169fe55ebd4dd5f
Ubuntu <ubuntu@ip-172-31-21-22.ec2.internal>
2022-04-01 22:36:24 +0000

    cool code is gucci

--- number_guessing.py
+++ number_guessing.py
@@ -13,17 +13,36 @@ def main():
     # and tells the user if they guessed corretly
 
     # input
-    user_guess = int(input("Insert any number between 0-9 (integers): "))
-    number = random.randint(1, 9)
+    user_guess = int(input("Insert any number between 0-12 (integers): "))
+    print("")
 
     # process and output
-    print("")
-    if user_guess == number:
-        print("Hooray you guessed correctly !! :)")
+    if user_guess == 1:
+        print("The month is January")
+    elif user_guess == 2:
+        print("The month is February")
+    elif user_guess == 3:
+        print("The month is March")
+    elif user_guess == 4:
+        print("The month is April")
+    elif user_guess == 5:
+        print("The month is May")
+    elif user_guess == 6:
+        print("The month is June")
+    elif user_guess == 7:
+        print("The month is July")
+    elif user_guess == 8:
+        print("The month is August")
+    elif user_guess == 9:
+        print("The month is September")
+    elif user_guess == 10:
+        print("The month is October")
+    elif user_guess == 11:
+        print("The month is November")
+    elif user_guess == 12:
+        print("The month is December")
     else:
-        print("Oh No!!! you guessed incorrectly :(")
-
-    print("\nDone.")
+        print("Sorry big man I got nothing ¯\_(ツ)_/¯ ")
 
 
 if __name__ == "__main__":
